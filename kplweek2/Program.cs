2574f9131b1063b48302c076158ab077c74e2985
unknown <f.amalia111@gmail.com>
2024-02-27 15:07:11 +0700

    inisialisasi program.cs

--- kplweek2/Program.cs
+++ kplweek2/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.Write ("Masukkan nama Anda: ");
+String namaMahasiswa = Console.ReadLine();
 Console.WriteLine("Hello, World!");
+Console.WriteLine($"Hello, {namaMahasiswa}!");
 
